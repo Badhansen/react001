5241167035a3ce746d0ef6fb942af7b427941a51
Badhan Sen <badhan.cse@gmail.com>
2024-03-06 21:07:43 +0000

    event handler

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,9 +2,18 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Ireland", "India", "Bangladesh", "USA"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectedItem}
+      />
     </div>
   );
 }
