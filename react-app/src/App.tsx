c0cb7d2f10546432bcdabb96f5d72a39caa6834a
Badhan Sen <badhan.cse@gmail.com>
2024-03-06 20:54:56 +0000

    Props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Ireland", "India", "Bangladesh", "USA"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
