d79f904a186a5b017a9cabba1223e537990416e9
Badhan Sen <badhan.cse@gmail.com>
2024-03-07 18:35:41 +0000

    Alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,26 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 
 function App() {
-  let items = ["Ireland", "India", "Bangladesh", "USA"];
+  // let items = ["Ireland", "India", "Bangladesh", "USA"];
 
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
+    // <div>
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectedItem}
+    //   />
+    // </div>
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      />
+      <Alert>
+        Hello world! <span>Badhan Sen</span>
+      </Alert>
     </div>
   );
 }
